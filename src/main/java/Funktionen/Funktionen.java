a5f78c13ffceda6a28ddf19088bbe9a8ca651188
Frank <mcfranzl26@gmail.com>
2022-05-23 09:59:34 +0200

    faktorisieren + rsacrypt

--- src/main/java/Funktionen/Funktionen.java
+++ src/main/java/Funktionen/Funktionen.java
@@ -1,22 +1,22 @@
 package Funktionen;
 
 public class Funktionen {
-    public static int euklidischerAlgorithmus(int r0, int r1) {
+    public static long euklidischerAlgorithmus(long r0, long r1) {
         return euklidischerAlgorithmus(r0, r1, false);
     }
 
-    public static int euklidischerAlgorithmus(int r0, int r1, boolean ausgabe) {
+    public static long euklidischerAlgorithmus(long r0, long r1, boolean ausgabe) {
         if (r0 < 1 || r1 < 1)
             throw new IllegalArgumentException("r0 und r1 muessen positive ganze Zahlen sein");
         if (r0 < r1) {
-            int tmp = r0;
+            long tmp = r0;
             r0 = r1;
             r1 = tmp;
         }
-        int stellen = (int) (Math.log10(r0) + 1);
+        long stellen = (int) (Math.log10(r0) + 1);
 
         do {
-            int tmp = r0 % r1;
+            long tmp = r0 % r1;
             if (ausgabe) System.out.printf("%" + stellen + "d = %d * %" + stellen + "d + %d\n", r0, r0 / r1, r1, tmp);
             r0 = r1;
             r1 = tmp;
@@ -24,7 +24,7 @@ public class Funktionen {
         return r0;
     }
 
-    public static int[] erweiterterEuklidischerAlgorithmus(int r0, int r1) {
+    public static long[] erweiterterEuklidischerAlgorithmus(long r0, long r1) {
         return erweiterterEuklidischerAlgorithmus(r0, r1, false);
     }
 
@@ -32,21 +32,21 @@ public class Funktionen {
      * @param ausgabe Erweiterte Ausgabe / Lösungsweg
      * @return Integer-Array mit 3 Werten: ggT(r0,r1); s; t mit 1 = s * r0 + t * r1
      */
-    public static int[] erweiterterEuklidischerAlgorithmus(int r0, int r1, boolean ausgabe) {
+    public static long[] erweiterterEuklidischerAlgorithmus(long r0, long r1, boolean ausgabe) {
         if (r0 < 1 || r1 < 1)
             throw new IllegalArgumentException("r0 und r1 muessen positive ganze Zahlen sein");
         if (r0 < r1)
             throw new IllegalArgumentException("r0 muss groesser als r1 sein");
-        int r0alt = r0;
-        int r1alt = r1;
-        int s0 = 1, s1 = 0, t0 = 0, t1 = 1;
-        int stellen = (int) (Math.log10(r0) + 1);
+        long r0alt = r0;
+        long r1alt = r1;
+        long s0 = 1, s1 = 0, t0 = 0, t1 = 1;
+        long stellen = (int) (Math.log10(r0) + 1);
         do {
             // neue Variablen Berechnen
-            int r2 = r0 % r1;
-            int q = r0 / r1;
-            int s2 = s0 - q * s1;
-            int t2 = t0 - q * t1;
+            long r2 = r0 % r1;
+            long q = r0 / r1;
+            long s2 = s0 - q * s1;
+            long t2 = t0 - q * t1;
 
             if (ausgabe)
                 System.out.printf("%" + stellen + "d = %d * %" + stellen + "d + %" + stellen + "d", r0, r0 / r1, r1, r2);
@@ -60,10 +60,10 @@ public class Funktionen {
             t0 = t1;
             t1 = t2;
         } while (r1 != 0);
-        return new int[]{r0, s0, t0};
+        return new long[]{r0, s0, t0};
     }
 
-    public static int inverseBerechnen(int zahl, int moduloRaum) {
+    public static long inverseBerechnen(long zahl, long moduloRaum) {
         return inverseBerechnen(zahl, moduloRaum, false);
     }
 
@@ -71,15 +71,45 @@ public class Funktionen {
      * @param ausgabe erweiterte Ausgabe / Lösungsweg
      * @return zahl^-1 mod moduloRaum
      */
-    public static int inverseBerechnen(int zahl, int moduloRaum, boolean ausgabe) {
+    public static long inverseBerechnen(long zahl, long moduloRaum, boolean ausgabe) {
         if (zahl < 1 || moduloRaum < 1)
             throw new IllegalArgumentException("Beide Parameter muessen groesser 0 sein");
         if (zahl > moduloRaum)
             throw new IllegalArgumentException("Zahl muss groesser als der ModuloRaum sein");
-        int[] ergebnis = erweiterterEuklidischerAlgorithmus(moduloRaum, zahl, ausgabe);
+        long[] ergebnis = erweiterterEuklidischerAlgorithmus(moduloRaum, zahl, ausgabe);
         if (ergebnis[0] != 1)
             throw new IllegalArgumentException("der GGT der Parameter ist nicht 1 und damit keine Inverse bestimmbar");
         if (ergebnis[2] < 0) ergebnis[2] += moduloRaum;
         return ergebnis[2];
+    }
+
+    public static String faktorisieren(long zahl) {
+        StringBuilder s = new StringBuilder();
+        while (zahl != 1) {
+            for (int i = 2;; i++) {
+                if (zahl % i == 0) {
+                    if (!s.isEmpty()) s.append(" * ");
+                    s.append(i);
+                    zahl /= i;
+                    break;
+                }
+                if (i > zahl / 2) {
+                    if (!s.isEmpty()) s.append(" * ");
+                    s.append(zahl);
+                    zahl = 1;
+                    break;
+                }
+            }
+        }
+
+        return s.toString();
+    }
+
+    public static long[] rsaCrypt(long[] werte, long key, long modus) {
+        long[] ergebnis = new long[werte.length];
+        for (int i = 0; i < werte.length; i++) {
+            ergebnis[i] = (werte[i] ^ key) % modus;
+        }
+        return ergebnis;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,35 @@ import static Funktionen.Funktionen.*;
 
 public class Main {
     public static void main(String[] args) {
+        // test();
+        uebung5();
+    }
+
+    public static void uebung5() {
+        // Aufgabe 2
+        System.out.println("Aufgabe 2:");
+        System.out.println("2002: " + faktorisieren(2002));
+        // System.out.println("1004716607: " + faktorisieren(1004716607)); // ist Prim!
+        System.out.println("1004716607: " + "1004716607");
+        System.out.println("1004716608: " + faktorisieren(1004716608));
+
+        // Aufgabe 3
+        System.out.println("\nAufgabe 3: ");
+
+        // Aufgabe 4
+        System.out.println("\nAufgabe 4: "); // phi(21) = (3-1)*(7-1) = 12
+        System.out.println("5^-1 mod 12: " + inverseBerechnen(5, 12));
+        long[] ergebnis = rsaCrypt(new long[]{3, 1, 13, 4, 7, 0, 4, 11, 6, 1, 8, 13, 4, 10}, 5, 21);
+        System.out.println("a: " + Arrays.toString(ergebnis));
+        ergebnis = rsaCrypt(new long[]{18, 1, 2, 3, 18, 16, 19, 3, 11, 6, 0, 17, 16, 3, 7, 11, 6, 13, 16, 13, 14}, 5, 21);
+        System.out.println("b: " + Arrays.toString(ergebnis));
+    }
+
+    public static void test() {
         System.out.println(euklidischerAlgorithmus(973, 301, true));
         System.out.println(Arrays.toString(erweiterterEuklidischerAlgorithmus(973, 301, true)));
         System.out.println(Arrays.toString(erweiterterEuklidischerAlgorithmus(67, 12, true)));
-        System.out.println(inverseBerechnen(7,12, true));
+        System.out.println(inverseBerechnen(7, 12, true));
+        System.out.println(faktorisieren(976));
     }
-
 }
